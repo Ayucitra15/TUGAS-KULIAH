1297191f88d4aa080d1e162a6e40a1ddf97bcb8c
Ayu Citra Lestari <ayucitra4417@gmail.com>
2022-04-26 20:22:05 +0700

    get data

--- app.js
+++ app.js
@@ -27,3 +27,19 @@ app.post('/api/movies', (req, res) => {
 
 
 app.listen(PORT, () => console.log(`Server running at port: ${PORT}`));
+// read data / get data
+app.get('/api/movies', (req, res) => {
+    // buat query sql
+    const querySql = 'SELECT * FROM movies';
+
+    // jalankan query
+    koneksi.query(querySql, (err, rows, field) => {
+        // error handling
+        if (err) {
+            return res.status(500).json({ message: 'Ada kesalahan', error: err });
+        }
+
+        // jika request berhasil
+        res.status(200).json({ success: true, data: rows });
+    });
+});
